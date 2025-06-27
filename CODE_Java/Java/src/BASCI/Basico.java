4164dec897f5a8c44a313bb2bbcbc7e5e1ad4957
nereazj544 <nzjvirtual@gmail.com>
2025-06-27 14:36:59 +0200

    Agregar clase Basico con método main y entrada de usuario

---
+++ CODE_Java/Java/src/BASCI/Basico.java
@@ -0,0 +1,17 @@
+package BASCI;
+
+import java.util.Scanner;
+
+public class Basico {
+    public static void main(String[] args) {
+        System.out.println("Escribe un numero: ");
+        
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int num = sc.nextInt();
+
+        System.out.println("Tu b");
+
+    }
+}
