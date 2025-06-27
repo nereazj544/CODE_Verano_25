10575a3de05b71545cfbdc82d8b2b7c2a986f8d0
nereazj544 <nzjvirtual@gmail.com>
2025-06-27 14:52:32 +0200

    Mejorar la interacción del usuario en la clase Basico, corrigiendo mensajes y asegurando la validación de respuestas.

--- CODE_Java/Java/src/BASCI/Basico.java
+++ CODE_Java/Java/src/BASCI/Basico.java
@@ -5,13 +5,23 @@ import java.util.Scanner;
 public class Basico {
     public static void main(String[] args) {
         System.out.println("Escribe un numero: ");
-        
-        
+
         Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt(); // Registramos el numero que ingrese el user
+
+        System.out.println("Tu numero es: 3, ¿si o no?");
+
+        String rs = sc.next(); // Registramos la respuesta del usuario
+
+        if (rs.equals("si") || rs.equals("s") || rs.equals("yes") || rs.equals("y")) {
+            System.out.println("Tu numero es 3");
+        } else if (rs.equals("no") || rs.equals("n")) {
+            System.out.println("Tu numero no es 3, entinces es: " + num);
+        } else {
+            System.out.println("Respuesta no valida");
+        }
         
-        int num = sc.nextInt();
-
-        System.out.println("Tu b");
-
+        sc.close();
     }
 }
