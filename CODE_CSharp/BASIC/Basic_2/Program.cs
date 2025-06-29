5ba19065ee616997717b01ab67b2cbae15e62952
Nerea ZJ <117531760+nereazj544@users.noreply.github.com>
2025-06-29 18:26:13 +0200

    Add files via upload
    Add initial build artifacts and project configuration for Basic_2
    
    - Created Basic_2.dll and Basic_2.ref.dll for the project.
    - Added project.assets.json to manage dependencies and project settings.
    - Included project.nuget.cache for NuGet package management.
    - Configured target framework to .NET 8.0 with appropriate imports and references.

---
+++ CODE_CSharp/BASIC/Basic_2/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+//TODO: 1 . Diferencias enre WriteLine y Wirte
+
+Console.WriteLine("¡Hola, Mundo!");
+Console.WriteLine("C# es un lenguaje de programación orientado a objetos.");
+Console.Write("SIN ENTER AL FINAL");
+Console.Write("SIN ENTER AL FINAL");
+
+
+
+Console.WriteLine("\n ================= \n");  // La "\n" es para saltar una línea
+
+
+//TODO: 2. MESNAJES Y LEER INFORMACION POR CONSOLA
+
+Console.WriteLine("¿Cual es tu nombre?"); //Solicitar el nombre
+string name = Console.ReadLine(); //Recibir el nombre
+Console.WriteLine($"¿Tu nombre es: {name}? (s/n)"); //Preguntar si es correcto
+string respuesta = Console.ReadLine(); //Recibir la respuesta
+
+//Flujo de control If-Else 
+if (respuesta.Equals("s")) //Comprobar si la respuesta es "s"
+{
+    Console.WriteLine($"Bienvenid@ {name}");
+}
+else if(respuesta.Equals("n")) //Comprobar si la respuesta es "n"
+{
+    Console.WriteLine("¿Cual es tu nombre?"); //Solicita de nuevo el nombre
+    name = Console.ReadLine(); //Recibe el nuevo nombre
+    Console.WriteLine($"Bienvenid@ {name}");
+} else //En caso de que la respuesta no sea "s" o "n"
+{
+    Console.WriteLine("Respuesta no válida. Por favor, responde con 's' o 'n'."); 
+    //Es un mensaje por defecto
+}
