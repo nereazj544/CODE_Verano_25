10b732593d568264bb1fe3ccfa08ad90edbe3813
nereazj544 <nzjvirtual@gmail.com>
2025-07-01 17:20:39 +0200

    update proyect

---
+++ CODE_CSharp/Basic3_Funciones/FileName.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic3_Funciones
+{
+    internal class FileName
+    {
+    }
+}
---
+++ CODE_CSharp/Basic3_Funciones/Program.cs
@@ -0,0 +1,46 @@
+﻿//LIBRERIAS
+using System;
+
+// NOMBRE DEL ESPACIO 
+namespace Basic3_Funciones
+{
+    class Program //Nombre de la clase
+    {
+        public static void Saludo() // Declaracion de una funcion (sin pasar parametros)
+        {
+            Console.WriteLine("=== METODO SIN PARAMETROS ===");
+            Console.WriteLine("Hola usuario"); //Imprimir en la consola el saludo
+            Console.WriteLine("\n=== === ===\n");
+
+        }
+
+        private static void Saludo_PR(string name) // Declaracion de una funcion (pasando parametros)
+        {
+            {
+                Console.WriteLine("=== METODO CON PARAMETROS ===");
+                Console.WriteLine($"Hola usuario: {name}"); //Imprimir en la consola el saludo
+                Console.WriteLine("\n=== === ===\n");
+            }
+
+        }
+
+        static void Main() //Funcion principal donde se ejecuta todo el programa
+        {
+            //TODO: FUNCION SIN PARAMETROS
+            //Llamar a la funcion Saludo
+            Saludo(); //No se le pasan parametros
+
+
+            //TODO: FUNCION CON PARAMETROS
+
+            Console.Write("Tu nombre: ");
+            string name = Console.ReadLine();
+
+            //Llamar a la funcion Saludo_PR
+            Saludo_PR(name); //A esta funcion se le pasa un parametro de tipo STRING (se pueden pasar otros tipos de datos)
+
+        }
+
+
+    }
+}
