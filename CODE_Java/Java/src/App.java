368bbd12762eb51f530997ad88dbfc0cdf11c57f
nereazj544 <nzjvirtual@gmail.com>
2025-07-16 16:40:19 +0200

    Agregar estructura básica de HTML y comentarios en App.java

--- CODE_Java/Java/src/App.java
+++ CODE_Java/Java/src/App.java
@@ -2,4 +2,9 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+    //TODO: PENDIENTES
+    //?
+    //* */
+    //!
+    
 }
