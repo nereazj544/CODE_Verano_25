ffbac04138a85568f4d522e49aac8391c3104c35
Nerea ZJ <117531760+nereazj544@users.noreply.github.com>
2025-08-11 14:00:54 +0200

    .

---
+++ CODE_CSharp/_POO/Tets_1_Poo/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tets_1_Poo
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int  edad { get; set; }
+
+        //== CONSTRUCTOR ==
+        public Person(string nombre, int age)
+        {
+        Name = nombre;
+            edad = age;
+        }
+
+        public void Mostrar()
+        {
+            Console.WriteLine($"Nombre: {Name}, Edad: {edad}");
+        }
+
+    }
+}
---
+++ CODE_CSharp/_POO/Tets_1_Poo/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tets_1_Poo;
+
+class Program
+{
+    static void Main ()
+    {
+        Console.WriteLine( "¿Cual es tu nombre?" );
+        string  nombre = Console.ReadLine();
+        Console.WriteLine( "¿Cual es tu edad?" );
+        int  edad = Convert.ToInt32(Console.ReadLine());
+
+        
+        Person pr = new Person(nombre, edad);
+        pr.Mostrar();
+
+
+        /*
+        Person pr = new Person("Neo", 20);
+        Person pr1 = new Person("Kai", 5); 
+        pr.Mostrar();
+        pr1.Mostrar();
+        */
+        //! Sin constructor
+        //pr.Name = "Neo";
+        //pr.edad = 20;
+
+       
+    }
+}
